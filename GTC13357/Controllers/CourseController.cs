60e6479379043ae43df3c8dafbe1fb33249ec2f4
murasame112 <79984587+murasame112@users.noreply.github.com>
2021-10-25 19:05:48 +0200

    delete and edit

--- GTC13357/Controllers/CourseController.cs
+++ GTC13357/Controllers/CourseController.cs
@@ -10,8 +10,39 @@ namespace gtc13357.Controllers
 {
     public class CourseController : Controller
     {
-        static List<Course> courses = new List<Course>();
-        
+        static List<Course> courses = new List<Course>()
+        {
+            new Course(){Id = 1, Name="Jim Reynor", Type="Begginer", Hours=36} ,
+            new Course(){Id = 2, Name="Sarah Kerrigan", Type="Pro", Hours=160},
+            new Course(){Id = 3, Name="Arcturus Mengsk", Type="Intermediate", Hours=96}
+        };
+
+        private static int index = 3;
+
+        [HttpPost]
+        public IActionResult Edit(Course editedCourse)
+        {
+            int id = editedCourse.Id;
+            Course originalCourse = courses.Find(course => course.Id == id);
+            originalCourse.Name = editedCourse.Name;
+            originalCourse.Type = editedCourse.Type;
+            originalCourse.Hours = editedCourse.Hours;
+            return View("List", courses);
+
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Course findCourse = courses.Find(course => course.Id == id);
+            return View("EditForm", findCourse);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Course findCourse = courses.Find(course => course.Id == id);
+            courses.RemoveAt(id-1);
+            return View("List", courses);
+        }
 
         public IActionResult Index()
         {
@@ -33,8 +64,17 @@ namespace gtc13357.Controllers
         [HttpPost]
         public IActionResult Add(Course course)
         {
-            courses.Add(course);
-            return View("ConfirmCourse", course);
+            if (ModelState.IsValid)
+            {
+                index++;
+                course.Id = index;
+                courses.Add(course);
+                return View("ConfirmCourse", course);
+            }
+            else
+            {
+                return View("AddForm");
+            }
         }
         
     }
--- GTC13357/Models/Course.cs
+++ GTC13357/Models/Course.cs
@@ -2,13 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace GTC13357.Models
 {
     public class Course
     {
+        [HiddenInput]
+        public int Id { get; set; }
+        [Required(ErrorMessage="Name is required!")]
+        [MinLength(2)]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Type is required!")]
         public string Type { get; set; }
+        [Range(minimum: 6, maximum: 180, ErrorMessage = "You have to choose a number between 6 and 180 hours!")]
         public int Hours { get; set; }
 
     }
