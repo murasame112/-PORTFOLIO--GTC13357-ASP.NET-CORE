3bbfa4cd431ad81666fe12682832b3225c795e95
murasame112 <79984587+murasame112@users.noreply.github.com>
2021-12-20 13:29:25 +0100

    20.12, punkty do 12 zrobione, nalezy dodac view Login (chyba?)

--- GTC13357/Controllers/AccountController.cs
+++ GTC13357/Controllers/AccountController.cs
@@ -18,6 +18,53 @@ namespace gtc13357.Controllers
     {
 
         private UserManager<IdentityUser> _userManager;
-        //skonczylem w punkcie 8
+        private SignInManager<IdentityUser> _signInManager;
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginModel
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = await
+                _userManager.FindByNameAsync(loginModel.Name);
+                if (user != null)
+                {
+                    await _signInManager.SignOutAsync();
+                    if ((await _signInManager.PasswordSignInAsync(user,
+                    loginModel.Password, false, false)).Succeeded)
+                    {
+                        return Redirect(loginModel?.ReturnUrl ?? "/Admin/Index");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Nieprawid³owa nazwa u¿ytkownika lub has³o");
+            return View(loginModel);
+
+        }
+
+
+        public async Task<RedirectResult> Logout(string returnUrl = "/")
+        {
+            await _signInManager.SignOutAsync();
+            return Redirect(returnUrl);
+        }
     }
+
 }
---
+++ GTC13357/Models/LoginModel.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.Extensions.Caching.Memory;
+using System.ComponentModel;
+
+
+namespace GTC13357.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [UIHint("password")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+
+    }
+
+
+}
