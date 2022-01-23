d5ede8d88297054577725367bb4ceaf97f29db1b
murasame112 <79984587+murasame112@users.noreply.github.com>
2022-01-23 01:05:20 +0100

    23.01 koniec pracy nad testami

--- GTC13357Test/ApiCourseControllerTest.cs
+++ GTC13357Test/ApiCourseControllerTest.cs
@@ -14,6 +14,7 @@ namespace gtc13357Test
 
         public ICRUDCourseRepository courses;
         
+
         public ApiCourseControllerTest(ICRUDCourseRepository courses)
         {
             this.courses = courses;
